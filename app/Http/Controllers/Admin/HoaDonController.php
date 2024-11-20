bf4cb0f4249878c15dd4f9fbfe3a20a0f44fb832
dait17 <daitr171003@gmail.com>
2024-11-20 16:17:59 +0700

    - fix controller HoaDonController

--- app/Http/Controllers/Admin/HoaDonController.php
+++ app/Http/Controllers/Admin/HoaDonController.php
@@ -7,12 +7,64 @@ use Illuminate\Http\Request;
 
 class HoaDonController extends Controller
 {
-    function index()
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-        return view('admin.HoaDon');
+        return view('admin.HoaDon.index');
     }
+
     public function inds()
     {
         return view('admin.HoaDon.inds');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
